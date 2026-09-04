f9cd1171d966f03ef71fdabb2acf65b246428482
Andrei Ivan <iamandrewtech@gmail.com>
2026-09-04 13:51:34 +0300

    add footer and homepage

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, redirect, render_template, session, request
 from flask_session import Session
-import sqlite3, random, re, werkzeug.security
+import sqlite3, random, re, werkzeug.security, datetime, requests
 
 # Initialize application
 app = Flask(__name__)
 
 # Define constants
 USER_TYPES = ['admin', 'user']
+OMDB_API = "d220b5ff"
 
 # Configure session cookies 
 app.secret_key = random.randbytes(8)
@@ -14,15 +15,38 @@ app.config['SESSION_TYPE'] = "filesystem"
 app.config['SESSION_PERMANENT'] = False
 Session(app)
 
-@app.route("/")
+@app.template_filter('limit')
+def limit(description):
+    if len(description) > 400:
+        return description[:401] + "..."
+    return description
+
+@app.route("/", methods=['GET'])
 def index():
     if session.get("user_id"):
+        offset = 0
         conn = sqlite3.connect("wwmt.db")
         curr = conn.cursor()
         record = curr.execute("SELECT * FROM users WHERE id=?", (session.get("user_id"),)).fetchone()
+        if not record:
+            session.clear()
+            return render_template("error.html", error="Invalid user ID. Please log in again.")
+        # Get user type and username
         user = record[4]
         username = record[1]
-        return render_template("home.html", user=user, username=username)
+        # Define offset
+        offset = 0
+        if request.args.get("page"):
+            try:
+                page = int(request.args.get("page"))
+            except ValueError:
+                page = 0
+            if page < 0:
+                page = 0
+            offset = 10 * page
+        movies = curr.execute("SELECT * FROM movies LIMIT 10 OFFSET ?", (offset,)).fetchall()
+        
+        return render_template("home.html", user=user, username=username, movies=movies)
     else:
         return render_template("landing_page.html")
 
@@ -60,18 +84,23 @@ def register():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    # If user is not logged in, redirect
     if session.get("user_id"):
         return redirect("/")
     if request.method == "POST":
+        # Check for invalid input
         if '' in [request.form.get(field) for field in request.form]:
             return render_template("error.html", error="Invalid input. ")
         if not re.match(r"^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$", request.form.get("email")):
             return render_template("error.html", error="Invalid email. ")
+        # Connect to database
         conn = sqlite3.connect("./wwmt.db")
         curr = conn.cursor()
+        # Check if user exists
         records = curr.execute("SELECT * FROM users WHERE email=?", (request.form.get("email"),)).fetchall()
         if len(records) == 0:
             return render_template("error.html", error='Email not registered. ')
+        # Check password validity
         password = records[0][3]
         if not werkzeug.security.check_password_hash(password, request.form.get("password")):
             return render_template("error.html", error="Incorrect password. ")
