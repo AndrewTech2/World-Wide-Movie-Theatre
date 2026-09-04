236ebf5148ff9c69a3008236eb446091cce41de8
Andrei Ivan <iamandrewtech@gmail.com>
2026-09-04 03:00:34 +0300

    add login system and homepage for registered users

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import sqlite3, random, re, werkzeug.security
 # Initialize application
 app = Flask(__name__)
 
+# Define constants
+USER_TYPES = ['admin', 'user']
+
 # Configure session cookies 
 app.secret_key = random.randbytes(8)
 app.config['SESSION_TYPE'] = "filesystem"
@@ -14,7 +17,12 @@ Session(app)
 @app.route("/")
 def index():
     if session.get("user_id"):
-        return render_template("home.html")
+        conn = sqlite3.connect("wwmt.db")
+        curr = conn.cursor()
+        record = curr.execute("SELECT * FROM users WHERE id=?", (session.get("user_id"),)).fetchone()
+        user = record[4]
+        username = record[1]
+        return render_template("home.html", user=user, username=username)
     else:
         return render_template("landing_page.html")
 
@@ -29,6 +37,8 @@ def register():
         # Ensure email is valid
         if not re.match(r'^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$', request.form.get("email")):
             return render_template("error.html", error="Error: Invalid email. ")
+        if not request.form.get("account") in USER_TYPES:
+            return render_template("error.html", error="Error: Invalid account type. ")
         # Connect to database and check if user already exists
         conn = sqlite3.connect("./wwmt.db")
         curr = conn.cursor()
@@ -39,7 +49,7 @@ def register():
         else:
             # Hash the password and insert the new user into the database
             hsh = werkzeug.security.generate_password_hash(request.form.get("password"))
-            curr.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", (request.form.get("username"), request.form.get("email"), hsh,))
+            curr.execute("INSERT INTO users (username, email, password, account) VALUES (?, ?, ?, ?)", (request.form.get("username"), request.form.get("email"), hsh, request.form.get("account"),))
         # Set the user_id in the session and redirect to the home page
         session['user_id'] = curr.execute("SELECT id FROM users WHERE username = ?", (request.form.get("username"),)).fetchall()[0][0]
         conn.commit()
@@ -47,3 +57,27 @@ def register():
         return redirect("/")
     else:
         return render_template("register.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if session.get("user_id"):
+        return redirect("/")
+    if request.method == "POST":
+        if '' in [request.form.get(field) for field in request.form]:
+            return render_template("error.html", error="Invalid input. ")
+        if not re.match(r"^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$", request.form.get("email")):
+            return render_template("error.html", error="Invalid email. ")
+        conn = sqlite3.connect("./wwmt.db")
+        curr = conn.cursor()
+        records = curr.execute("SELECT * FROM users WHERE email=?", (request.form.get("email"),)).fetchall()
+        if len(records) == 0:
+            return render_template("error.html", error='Email not registered. ')
+        password = records[0][3]
+        if not werkzeug.security.check_password_hash(password, request.form.get("password")):
+            return render_template("error.html", error="Incorrect password. ")
+        session['user_id'] = records[0][0]
+        return redirect("/")
+        conn.commit()
+        conn.close()
+    else:
+        return render_template("login.html")
